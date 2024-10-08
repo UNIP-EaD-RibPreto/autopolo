d665570ba020e63e4063aa90b0c5d0426305daaa
Sidney <sidney.moretto@gmail.com>
2024-10-08 10:49:24 -0300

    inicial

---
+++ scriptComplet.py
@@ -0,0 +1,19 @@
+# %% [markdown]
+# #Controle do Funil Polo
+
+# %%
+import selenium
+import pyautogui
+
+# %%
+print("Insira seu usuário")
+login = input("")
+
+print("Qual sua senha do SIAP:")
+senha = input("")
+
+# %%
+print(login)
+print(senha)
+
+
